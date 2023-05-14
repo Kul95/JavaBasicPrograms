832f2053d2e77555d84ad5605dc1327586e2900a
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-14 20:27:24 +0530

    Added: Leap year program

--- src/com/Bridglabz/BasicCorePrograms/FlipCoin.java
+++ src/com/Bridglabz/BasicCorePrograms/FlipCoin.java
@@ -1,11 +1,27 @@
 package com.Bridglabz.BasicCorePrograms;
 
+import java.util.Scanner;
+
 public class FlipCoin {
     public static void main(String[] args) {
+
+       coin();
+    }
+    public static void coin(){
         if(Math.random()<0.5){
             System.out.println("Head");
         }else{
             System.out.println("Tails");
+        }
+    }
+    public static void leapYear(){
+        Scanner sc=new Scanner(System.in);
+        int year=sc.nextInt();
+        System.out.println("Enter the year:");
+        if(((year%4==0) && (year%100 !=0))||(year%400==0)){
+            System.out.println("Leap Year");
+        }else{
+            System.out.println("Not a Leap Year");
         }
     }
 }
